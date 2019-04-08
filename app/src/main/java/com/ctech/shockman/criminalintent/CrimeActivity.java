6b625f1fb1d1f94337d380679687ca15a661f905
SamShoc <7794501@isd535.org>
2019-04-08 08:50:26 -0500

    Added Fragment classes and changed main activty to crime actity along with having crime activity inherite stuff

---
+++ app/src/main/java/com/ctech/shockman/criminalintent/CrimeActivity.java
@@ -0,0 +1,10 @@
+package com.ctech.shockman.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
