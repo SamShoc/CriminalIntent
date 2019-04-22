784cea36c30fa85753add599b79a952c2dcabc50
SamShoc <7794501@isd535.org>
2019-04-22 08:01:32 -0500

    Altered the tool bar

--- app/src/main/java/com/ctech/shockman/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/ctech/shockman/criminalintent/CrimeListFragment.java
@@ -9,6 +9,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -25,6 +27,13 @@ public class CrimeListFragment extends Fragment {
     private CrimeAdapter mAdapter;
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment_crime_list, container, false);
         mCrimeRecyclerView = myView.findViewById(R.id.crime_recycle_view);
@@ -35,9 +44,16 @@ public class CrimeListFragment extends Fragment {
         return myView;
     }
 
+    @Override
     public void onResume() {
         super.onResume();
         updateUI();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
     }
 
     private void updateUI() {
