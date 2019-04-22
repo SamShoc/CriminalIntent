01fb04400ff1060aa9fda061321c183b8fa78a4a
SamShoc <7794501@isd535.org>
2019-04-22 08:38:07 -0500

    Finished toolbar, you can now add crimes from by pressing the plus button

--- app/src/main/java/com/ctech/shockman/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/ctech/shockman/criminalintent/CrimeListFragment.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -55,6 +56,21 @@ public class CrimeListFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_crime_list, menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_crime:
+                Crime newCrime = new Crime();
+                CrimeLab.get(getActivity()).addCrime(newCrime);
+                Intent myIntent = CrimePagerActivity.newIntent(getActivity(), newCrime.getId());
+                startActivity(myIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
